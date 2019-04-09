98924b0f0f1b637653213cbd8bdf663ece71e12c
Nate Jacoby <nate@webbernet.com.au>
2019-04-09 10:24:06 +1000

    Add route to add a file upload row to a form [skip ci]

--- app/controllers/frefnet/file_controller.rb
+++ app/controllers/frefnet/file_controller.rb
@@ -1,9 +1,14 @@
 module Frefnet
   class FileController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:presign_upload]
+
     def presign_upload
       @facade = PresignUploadFacade.new(params)
       render json: @facade.run
     end
+
+    def add_row; end
+
+    def delete_row; end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Frefnet::Engine.routes.draw do
   post 'file/presign_upload'
+  get 'file/add_row'
 end
