7452285a49e45df9384c1f90f37a2132699ef7a2
Martin Brennan <mjrbrennan@gmail.com>
2019-09-05 14:46:57 +1000

    Remove require_login skip action not working

--- app/controllers/frefnet/file_controller.rb
+++ app/controllers/frefnet/file_controller.rb
@@ -1,7 +1,6 @@
 module Frefnet
   class FileController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:presign_upload]
-    skip_before_action :require_login, only: [:presign_upload]
 
     def presign_upload
       @facade = PresignUploadFacade.new(params)
