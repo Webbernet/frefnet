b080cdbff0aea5bcc2c9c5d29b048070bd572f7e
Martin Brennan <mjrbrennan@gmail.com>
2019-09-05 14:31:09 +1000

    Try allow skip require login for upload

--- app/controllers/frefnet/file_controller.rb
+++ app/controllers/frefnet/file_controller.rb
@@ -2,6 +2,10 @@ module Frefnet
   class FileController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:presign_upload]
 
+    if @frefnet_file_presign_upload_anonymous
+      skip_before_action :require_login
+    end
+
     def presign_upload
       @facade = PresignUploadFacade.new(params)
       render json: @facade.run
