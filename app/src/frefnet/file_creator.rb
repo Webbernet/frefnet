5267ca615164f3c21653aabe9ec13d47a5c45310
jake-webbernet <jake@webbernet.com.au>
2020-05-13 22:10:52 +1000

    Update file_creator.rb

--- app/src/frefnet/file_creator.rb
+++ app/src/frefnet/file_creator.rb
@@ -2,13 +2,14 @@ module Frefnet
   # Creates the file in the system so it can actually be tracked,
   # and is able to determine the content-type from the file name.
   class FileCreator
-    def initialize(original_filename:)
+    def initialize(original_filename:, key_start_override: nil)
       @original_filename = original_filename
+      @key_start = key_start_override || 'file_storage/'
     end
 
     def run
       Frefnet::FileReference.create(
-        key: "file_storage/#{SecureRandom.uuid}",
+        key: "#{@key_start}#{SecureRandom.uuid}",
         content_type: ContentTypeFinder.run(@original_filename),
         original_filename: @original_filename
       )
